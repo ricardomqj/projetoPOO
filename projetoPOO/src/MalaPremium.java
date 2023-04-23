bbb4c8c5ec21f5f465d7b2d64f7c91407bc9604a
Ricardo Jesus <92188713+ricardomqj@users.noreply.github.com>
2023-04-23 02:31:36 +0100

    Classes Artigo, Sapatilha, SapatilhaUsada alteradas

--- projetoPOO/src/MalaPremium.java
+++ projetoPOO/src/MalaPremium.java
@@ -1,3 +1,2 @@
 public class MalaPremium extends Mala{
-
 }
--- projetoPOO/src/Sapatilha.java
+++ projetoPOO/src/Sapatilha.java
@@ -4,30 +4,26 @@ public class Sapatilha extends Artigo{
     private int tamanho;
     private boolean temAtacadores;
     private String cor;
-    private boolean premium; // se é premium ou não
-
+    
     public Sapatilha() {
         super();
         this.tamanho = 0;
         this.temAtacadores = false;
         this.cor = "n/a";
-        this.premium = false;
     }
 
-    public Sapatilha(String codBarras, Transportadora transp, int stock, int numOwners, int avlEstado, double precoBase, double precoAtual, LocalDate dataLancamento, int discount,int tamanho, boolean atacadores, String cor, boolean premium) {
-        super(codBarras, transp, stock, numOwners, avlEstado, precoBase, precoAtual, discount,dataLancamento);
+    public Sapatilha(String codBarras, int stock, LocalDate dataLancamento, Transportadora trans, double precoBase, int tamanho, boolean atacadores, String cor) {
+        super(codBarras, stock, dataLancamento, trans, precoBase);
         this.tamanho = tamanho;
         this.temAtacadores = atacadores;
         this.cor = cor;
-        this.premium = premium;
     }
 
     public Sapatilha(Sapatilha umaSapatilha) {
         super(umaSapatilha);
         this.tamanho = umaSapatilha.getTamanho();
-        this.temAtacadores = umaSapatilha.isTemAtacadores();
+        this.temAtacadores = umaSapatilha.getAtacadores();
         this.cor = umaSapatilha.getCor();
-        this.premium = umaSapatilha.isPremium();
     }
 
     public Sapatilha clone() {
@@ -36,27 +32,24 @@ public class Sapatilha extends Artigo{
 
     public boolean equals(Object o) {
         if(this == o) return true;
-        if(o == null | o.getClass()!= this.getClass()) return false;
+        if(o == null || o.getClass()!=this.getClass()) return false;
 
-        Sapatilha s = (Sapatilha) o;
-        return super.equals(s) && this.tamanho == s.getTamanho() &&
-                this.temAtacadores == ((Sapatilha) o).isTemAtacadores() &&
-                this.cor.equals(s.getCor()) && this.premium == s.isPremium();
+        Sapatilha tilha = (Sapatilha) o;
+        return super.equals(tilha) && this.tamanho == tilha.getTamanho() &&
+                this.temAtacadores == tilha.getAtacadores() && this.cor.equals(tilha.getCor());
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append(super.toString());
-        sb.append("Tamanho da sapatilha: ").append(this.tamanho).append("\n");
-        sb.append("Tem atacadores? ").append(this.isTemAtacadores()).append("\n");
-        sb.append("Cor da sapatilha: ").append(this.cor).append("\n");
-        sb.append("Sapatilha é premium? ").append(this.premium).append("\n");
+        sb.append(super.getCodBarras());
+        sb.append("Tamanho: ").append(this.tamanho).append("\n");
+        sb.append("Tem atacadores? ").append(this.temAtacadores).append("\n");
+        sb.append("Cor: ").append(this.cor).append("\n");
 
         return sb.toString();
     }
 
-    // getters e setters
     public int getTamanho() {
         return this.tamanho;
     }
@@ -65,7 +58,7 @@ public class Sapatilha extends Artigo{
         this.tamanho = tamanho;
     }
 
-    public boolean isTemAtacadores() {
+    public boolean getAtacadores() {
         return this.temAtacadores;
     }
 
@@ -81,11 +74,4 @@ public class Sapatilha extends Artigo{
         this.cor = cor;
     }
 
-    public boolean isPremium() {
-        return this.premium;
-    }
-
-    public void setPremium(boolean premium) {
-        this.premium = premium;
-    }
 }
