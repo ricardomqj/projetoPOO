169a1b8d778452be43f9bdcc7925467bd444a049
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2023-04-29 16:40:56 +0100

    ++ profit variable ++

--- projetoPOO/src/Utilizador.java
+++ projetoPOO/src/Utilizador.java
@@ -8,8 +8,10 @@ public class Utilizador {
     private String nome;
     private String morada;
     private String nif;
+    private double profit; // falta acabar - para fazer preciso de produtosVendidos - tendo isso fazer qq coisa para ir buscar o precobase de cada produto vendido
     private Map<String, Artigo> produtosAVenda; // identificado pelo codigo de barras(como key)
     private Map<String, Encomenda> encomendasFeitas;
+    private Map<String, Artigo> produtosVendidos; // falta acabar
 
     public Utilizador() {
         this.codigoSistema = "n/a";
@@ -17,18 +19,23 @@ public class Utilizador {
         this.nome = "n/a";
         this.morada = "n/a";
         this.nif = "n/a";
+        this.profit = 0.0;
         this.encomendasFeitas = new HashMap<>();
         this.produtosAVenda = new HashMap<>();
+        this.produtosVendidos = new HashMap<>();
     }
 
-    public Utilizador(String codSis, String email, String name, String morada, String nif, Map<String, Artigo> produtosAVendaArg, Map<String, Encomenda> encomendasFeitasArg) {
+    public Utilizador(String codSis, String email, String name, String morada, String nif, int profit, Map<String, Artigo> produtosAVendaArg, Map<String, Encomenda> encomendasFeitasArg,
+    Map<String, Artigo> produtosVendidosArg) {
         this.codigoSistema = codSis;
         this.email = email;
         this.nome = name;
         this.morada = morada;
         this.nif = nif;
+        this.profit = profit;
         this.produtosAVenda = produtosAVendaArg;
         this.encomendasFeitas = encomendasFeitasArg;
+        this.produtosVendidos = produtosVendidosArg;
     }
 
     public Utilizador(Utilizador umUtilizador) {
@@ -37,8 +44,10 @@ public class Utilizador {
         this.nome = umUtilizador.getNome();
         this.morada = umUtilizador.getMorada();
         this.nif = umUtilizador.getNif();
+        this.profit = umUtilizador.getProfit();
         this.produtosAVenda = umUtilizador.getProdutosAVenda();
         this.encomendasFeitas = umUtilizador.getEncomendasFeitas();
+        this.produtosVendidos = umUtilizador.getProdutosVendidos();
     }
 
     public Utilizador clone() {return new Utilizador(this);}
@@ -51,8 +60,8 @@ public class Utilizador {
         Utilizador user = (Utilizador) o;
         return this.codigoSistema.equals(user.getCodigoSistema()) && this.email.equals(user.getEmail()) &&
                 this.nome.equals(user.getNome()) && this.morada.equals(user.getMorada()) &&
-                this.nif.equals(user.getNif()) && this.produtosAVenda.equals(user.getProdutosAVenda()) &&
-                this.encomendasFeitas.equals(user.getEncomendasFeitas());
+                this.nif.equals(user.getNif()) && this.profit == user.getProfit() && this.produtosAVenda.equals(user.getProdutosAVenda()) &&
+                this.encomendasFeitas.equals(user.getEncomendasFeitas()) && this.produtosVendidos.equals(user.getProdutosVendidos());
     }
 
     public String toString() {
@@ -63,6 +72,7 @@ public class Utilizador {
         sb.append("Nome: ").append(this.nome).append("\n");
         sb.append("Morada: ").append(this.morada).append("\n");
         sb.append("NIF: ").append(this.nif).append("\n");
+        sb.append("Profit: ").append(this.profit).append("\n");
         sb.append("Lista de produtos à venda: ").append("\n");
         for(Artigo art : this.produtosAVenda.values()) {
             sb.append(art.toString()).append("\n");
@@ -71,9 +81,17 @@ public class Utilizador {
         for(Encomenda art : this.encomendasFeitas.values()) {
             sb.append(art.toString()).append("\n");
         }
+        sb.append("Lista de produtos vendidos: ").append("\n");
+        for(Artigo art : this.produtosVendidos.values()) {
+            sb.append(art.toString()).append("\n");
+        }
 
         return sb.toString();
     }
+
+    // METHODS
+
+    public double profitTotal;
 
     // getters e setters
     public String getCodigoSistema() {
@@ -116,6 +134,20 @@ public class Utilizador {
         this.nif = nif;
     }
 
+    public double getProfit() {
+        return this.profit;
+    }
+    public void setProfit() {
+        double localprofit = 0.0;
+
+        for (Map.Entry<String, Artigo> entry : this.produtosVendidos.entrySet()) {
+            String key = entry.getKey();
+            Artigo value = entry.getValue();
+
+            localprofit += value.getPrecoBase();
+        }
+    }
+
     public Map<String, Artigo> getProdutosAVenda() {
         return this.produtosAVenda.values().stream().collect(Collectors.toMap(Artigo::getCodBarras, Artigo::clone));
     }
@@ -130,5 +162,13 @@ public class Utilizador {
 
     public void setEncomendasFeitas(Map<String, Encomenda> encomendasFeitas) {
         this.encomendasFeitas = encomendasFeitas.values().stream().collect(Collectors.toMap(Encomenda :: getIdEncomenda, Encomenda :: clone));
+    }
+
+    public Map<String, Artigo> getProdutosVendidos() {
+        return this.produtosVendidos.values().stream().collect(Collectors.toMap(Artigo :: getCodBarras, Artigo :: clone));
+    }
+
+    public void setProdutosVendidos(Map<String, Artigo> produtosVendidos) {
+        this.produtosVendidos = produtosVendidos.values().stream().collect(Collectors.toMap(Artigo::getCodBarras, Artigo::clone));
     }
 }
