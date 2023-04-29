b96b766d0cb0717f882a2193048ee85c1175bddf
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2023-04-29 16:03:48 +0100

    Update ++ IdEncomenda

--- projetoPOO/src/Encomenda.java
+++ projetoPOO/src/Encomenda.java
@@ -9,6 +9,8 @@ public class Encomenda {
         FINALIZADO,
         EXPEDIDO,
     }
+    
+    private String id;
     private Utilizador user;
     private Map<String, Artigo> artigos;
     private String tamanho;
@@ -17,6 +19,7 @@ public class Encomenda {
     private LocalDate data;
 
     public Encomenda() {
+        this.id = "";
         this.user = null;
         this.artigos = new HashMap<>();
         this.tamanho = "";
@@ -25,7 +28,8 @@ public class Encomenda {
         this.data = LocalDate.now();
     }
 
-    public Encomenda(Utilizador user, Map<String, Artigo> artigos, String tamanho, double precoFinal, StatusEncomenda status, LocalDate data) {
+    public Encomenda(String id, Utilizador user, Map<String, Artigo> artigos, String tamanho, double precoFinal, StatusEncomenda status, LocalDate data) {
+        this.id = id;
         this.user = user.clone();
         this.artigos = this.artigos.values().stream().collect(Collectors.toMap(Artigo::getCodBarras, Artigo::clone));
         this.tamanho = tamanho;
@@ -35,13 +39,14 @@ public class Encomenda {
     }
 
     public Encomenda(Encomenda umaEncomenda) {
+        this.id = umaEncomenda.getIdEncomenda();
         this.user = umaEncomenda.getUser();
         this.artigos = umaEncomenda.getArtigos();
         this.tamanho = umaEncomenda.getTamanhoEncomenda();
         this.precoFinal = umaEncomenda.getPrecoFinal();
         this.status = umaEncomenda.getStatus();
         this.data = umaEncomenda.getData();
-        
+
     }
 
     public Encomenda clone() {
@@ -53,14 +58,15 @@ public class Encomenda {
         if(o == null || o.getClass() != this.getClass()) return false;
 
         Encomenda e = (Encomenda) o;
-        return this.user.equals(e.getUser()) && this.artigos.equals(e.getArtigos()) &&
-                this.tamanho.equals(e.getTamanhoEncomenda()) && this.precoFinal == getPrecoFinal() &&
+        return this.id.equals(e.getIdEncomenda()) && this.user.equals(e.getUser()) && this.artigos.equals(e.getArtigos()) &&
+                this.tamanho.equals(e.getTamanhoEncomenda()) && this.precoFinal == e.getPrecoFinal() &&
                 this.status.equals(e.getStatus()) && this.data.equals(e.getData());
     }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
+        sb.append("Id: ").append(this.id).append("\n");
         sb.append("Nome do utilizador: ").append(this.user.getNome()).append("\n");
         sb.append("Lista de artigos da encomenda: \n");
         for(Artigo artigo : this.artigos.values()) {
@@ -125,6 +131,14 @@ public class Encomenda {
 
     // GETTERS AND SETTERS
 
+    public String getIdEncomenda() {
+        return this.id;
+    }
+    
+    public void setIdEncomenda(String id) {
+        this.id = id;
+    }
+    
     public Utilizador getUser() {
         return this.user.clone();
     }
@@ -156,7 +170,7 @@ public class Encomenda {
     public void setPrecoFinal() {
         this.precoFinal = custoTotal() + custoTotalExpedicao();
     }
-    
+
     public StatusEncomenda getStatus() {
         return this.status;
     }
@@ -170,5 +184,5 @@ public class Encomenda {
     public void setData(LocalDate data) {
         this.data = data;
     }
-    
+
 }
